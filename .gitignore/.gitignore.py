0f8986bd6da7c7d1f3dc558471ad92162794db14
minakhb2 <minakhb2@naver.com>
2019-11-25 18:51:08 +0900

    My Django Girls app, first commit

---
+++ .gitignore/.gitignore.py
@@ -0,0 +1,9 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> *.pyc
+*~
+__pycache__
+myvenv
+db.sqlite3
+/static
+.DS_Store
